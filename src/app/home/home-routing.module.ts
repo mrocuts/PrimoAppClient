e51446a14ffac83efab8a80070261eeee0f8b3fa
johnquintero <johnjairoqp@gmail.com>
2020-09-18 15:22:04 -0500

    va de nuevo

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
 //aqui se debe implementar el guard
+//cambio
 const routes: Routes = [
   {
     path: '',
